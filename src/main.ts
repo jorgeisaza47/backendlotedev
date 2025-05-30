aaceb8b5c0a13d644ec6ea7d9a775dd6838522a0
jorgeisaza705@gmail.com <jorgeisaza705@gmail.com>
2025-05-29 20:40:43 -0500

    cambios en el main

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,27 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-// import { DateFormatInterceptor } from './interceptors/date-format.interceptor';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.setGlobalPrefix('api');
-//   app.useGlobalInterceptors(new DateFormatInterceptor());
-
-//   // Habilitar CORS
-//   app.enableCors({
-//     origin: 'http://localhost:4200', // Permite solicitudes desde este origen
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-//     credentials: true, // Permite el envío de credenciales (cookies, headers de autenticación)
-//   });
-
-//   await app.listen(3000);
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       transform: true,
-//       whitelist: true,
-//       forbidNonWhitelisted: true,
-//     }),
-//   );
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import * as express from 'express';
-import * as fs from 'fs';
+import { ValidationPipe } from '@nestjs/common';
+import { DateFormatInterceptor } from './interceptors/date-format.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new DateFormatInterceptor());
 
-  const angularDistPath = join(
-    __dirname,
-    '..',
-    '..',
-    'frontendlote',
-    'dist',
-    'frontendlote',
-    'browser',
-  ); // <-- cambia 'mi-app' según tu caso
-
-  app.useStaticAssets(angularDistPath);
-  app.setBaseViewsDir(angularDistPath);
-
-  // Manejo correcto de rutas Angular (SPA)
-  app.use('*', (req, res) => {
-    res.sendFile(join(angularDistPath, 'index.html'));
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Permite solicitudes desde este origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Permite el envío de credenciales (cookies, headers de autenticación)
   });
 
-  await app.listen(3001, '0.0.0.0');
+  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 }
 bootstrap();
